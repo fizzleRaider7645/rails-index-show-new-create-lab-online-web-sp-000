5be12d94c795459df1b8795876f8b790f9725ef2
Douglas Uretsky <douglasuretsky@gmail.com>
2019-01-11 18:16:52 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,6 @@
 class CouponsController < ApplicationController
   def create
     @coupon = Coupon.create(coupon_code: params[:copuon_code], store: params[:store])
-    redirect_to
+    redirect_to coupons_path(@coupon)
   end
 end
